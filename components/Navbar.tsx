862af66d8b61877996c3c44e49d83be08dd7fbb0
ankitmrmishra <ankitmrmishra1118@gmail.com>
2024-10-02 02:55:50 +0530

    implementing the dropdown for profile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Workflow, Menu, X } from "lucide-react";
+import { Workflow, Menu, X, MenuIcon } from "lucide-react";
 import React, { useState } from "react";
 
 import Link from "next/link";
@@ -10,6 +10,14 @@ import {
   RegisterLink,
   useKindeBrowserClient,
 } from "@kinde-oss/kinde-auth-nextjs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 import Image from "next/image";
 
@@ -38,11 +46,36 @@ export default function Navbar() {
             <NavItem href="/contact">Contact Us</NavItem>
             <NavItem href="/about">About</NavItem>
           </div>
-          <div className="hidden md:flex items-center space-x-2 ">
+          <div className="hidden md:flex align-middle items-center space-x-2 ">
+            {user && (
+              <div className="profile flex justify-center align-middle items-center gap-2 ">
+                <Image
+                  src={user.picture || "/path/to/default-image.jpg"}
+                  alt={user.given_name || "User"}
+                  width={100}
+                  height={100}
+                  className="rounded-full size-10"
+                />
+                {/* <span>
+                  {user?.given_name} {user.family_name}
+                </span> */}
+              </div>
+            )}
             {isAuthenticated ? (
-              <LogoutLink className="bg-black text-white p-2 rounded-lg">
-                Log Out
-              </LogoutLink>
+              <div className="">
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <MenuIcon />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>
+                      <LogoutLink>Log Out </LogoutLink>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
             ) : (
               <>
                 <LoginLink className="bg-black text-white p-2 rounded-lg">
@@ -53,27 +86,27 @@ export default function Navbar() {
                 </RegisterLink>
               </>
             )}
-            {user && (
-              <div className="profile flex justify-center align-middle items-center gap-2 ">
-                <Image
-                  src={user.picture || "/path/to/default-image.jpg"}
-                  alt={user.given_name || "User"}
-                  width={100}
-                  height={100}
-                  className="rounded-full size-14"
-                />
-                <span>
-                  {user?.given_name} {user.family_name}
-                </span>
-              </div>
-            )}
           </div>
 
           <button className="md:hidden" onClick={toggleMenu}>
-            {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+            {isMenuOpen ? (
+              <X size={24} />
+            ) : (
+              <div className="flex justify-center align-middle items-center">
+                <Image
+                  src={user?.picture || "/path/to/default-image.jpg"}
+                  alt={user?.given_name || "User"}
+                  width={100}
+                  height={100}
+                  className="rounded-full size-6"
+                />
+                <Menu size={24} />
+              </div>
+            )}
           </button>
         </div>
       </div>
+
       {isMenuOpen && (
         <div className="md:hidden mt-4">
           <div className="flex flex-col space-y-2">
@@ -92,7 +125,12 @@ export default function Navbar() {
           </div>
           <div className="mt-4 flex flex-col space-y-2">
             {isAuthenticated ? (
-              <LogoutLink>Log Out</LogoutLink>
+              <div className="flex justify-center align-middle items-center gap-3 text-white">
+                <LogoutLink className="bg-black p-2 rounded-lg">
+                  Log Out
+                </LogoutLink>
+                <div className="bg-black p-2 rounded-lg">Profile</div>
+              </div>
             ) : (
               <>
                 <LoginLink>Sign In</LoginLink>
